119f6c73f735ab9df951a0ff1940b274e2cbd859
Zoltan Pozsonyi <pozsonyizoli@gmail.com>
2025-06-13 10:06:43 +0200

    fix duplicate key in localstorage

--- pages/1_Lecture.py
+++ pages/1_Lecture.py
@@ -13,8 +13,6 @@ PROGRESS_KEY = 'lecture_progress'
 FINISHED_KEY = 'lecture_finished'
 localS = LocalStorage()
 
-
-
 def __get_lecture_id(lecture_name):
     return lecture_name[4:-3]
 
@@ -22,11 +20,12 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 materials_dir = os.path.join(script_dir, '..','materials')
 lecture_list = sorted(os.listdir(materials_dir))
 
-if localS.getItem(PROGRESS_KEY) is None or not isinstance(localS.getItem(PROGRESS_KEY), str):
+if localS.getItem(PROGRESS_KEY) is None:
     localS.setItem(PROGRESS_KEY, lecture_list[0])
+    localS.setItem(FINISHED_KEY, [])   
 
 def get_current_lecture():
-    stored_progress = localS.getItem(PROGRESS_KEY)
+    stored_progress = localS.getItem(PROGRESS_KEY) or []
     if stored_progress:
         return next((lec for lec in lecture_list if __get_lecture_id(lec) == stored_progress), lecture_list[0])
 
@@ -44,10 +43,10 @@ def toggle_lecture_done(lecture_name):
     localS.setItem(FINISHED_KEY, finished)
 
 def is_any_lecture_done():
-    return bool(localS.getItem(FINISHED_KEY))
+    return bool(localS.getItem(FINISHED_KEY) or [])
 
 def is_lecture_done(lecture_name):
-    return __get_lecture_id(lecture_name) in localS.getItem(FINISHED_KEY)
+    return __get_lecture_id(lecture_name) in (localS.getItem(FINISHED_KEY) or [])
 
 def is_first_lecture():
     return get_current_lecture() == lecture_list[0]
@@ -55,10 +54,6 @@ def is_first_lecture():
 def is_last_lecture():
     return get_current_lecture() == lecture_list[-1]
 
-if not localS.getItem(PROGRESS_KEY):
-    localS.setItem(PROGRESS_KEY, __get_lecture_id(lecture_list[0]))
-if localS.getItem(FINISHED_KEY) is None or not isinstance(localS.getItem(FINISHED_KEY), list):
-    localS.setItem(FINISHED_KEY, [])
 
 
 current_lecture = get_current_lecture().split('.')[0].split('_', maxsplit=2)[1]
